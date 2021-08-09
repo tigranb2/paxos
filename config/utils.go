15c252c7fc8a3c1c8be485ca33daa3284752adc6
Tigran Bantikyan <tigranbantikyan@gmail.com>
2021-08-09 15:40:40 -0400

    changed to net for ip parsing

--- config/utils.go
+++ config/utils.go
@@ -1,51 +1,53 @@
 package config
-// useful functions for config data
+
+//useful functions for config data
 
 import (
 	"log"
+	"net"
 	"strconv"
-	"strings"
 )
 
-func (configData *config) GetQuorum() int {
+//GetQuorum returns number of Acceptors needed for majority
+func (configData *Config) GetQuorum() int {
 	return configData.Acceptors.Count/2 + 1
 }
 
-// ParseNetworkRanges parses ip and port ranges and checks for valid input
-func (configData *config) ParseNetworkRanges() (CONNECTIONS []string) {
-	var err error
-	ipMin, ipMax, portMin, portMax := 0, 0, 0, 0
-
-	ipRange := strings.Split(configData.Acceptors.ipRange, "-")
-	if len(ipRange) > 1 {
-		ipMax, err = strconv.Atoi(ipRange[1])
+//ParseNetworkRanges parses ip and port ranges and checks for valid input
+func (configData *Config) ParseNetworkRanges() (connections []string) {
+	ip := net.ParseIP(configData.Acceptors.InitIP)
+	if ip == nil {
+		log.Fatalf("initIP is not a valid ipv4 address")
 	}
-	ipMin, err = strconv.Atoi(ipRange[0])
+
+	port, err := strconv.Atoi(configData.Acceptors.InitPort)
 	if err != nil {
-		log.Fatalf("ipRange not formatted correctly: %v", err)
+		log.Fatalf("initPort is not a valid port")
 	}
 
-	portRange := strings.Split(configData.Acceptors.portRange, "-")
-	if len(portRange) > 1 {
-		portMax, err = strconv.Atoi(portRange[1])
-	}
-	portMin, err = strconv.Atoi(portRange[0])
-	if err != nil {
-		log.Fatalf("portRange not formatted correctly: %v", err)
+	connections = append(connections, ip.String()+":"+strconv.Itoa(port))
+	for i := 1; i < configData.Acceptors.Count; i++ {
+		if configData.Acceptors.IncrementIP {
+			ip = nextIP(ip, 1)
+		}
+		if configData.Acceptors.IncrementPort {
+			port++
+		}
+
+		connections = append(connections, ip.String()+":"+strconv.Itoa(port))
 	}
 
-	if ipMax-ipMin < configData.Acceptors.Count {
-		log.Fatalf("ipRange too small")
-	}
-	if portMax-portMin < configData.Acceptors.Count {
-		log.Fatalf("portRange too small")
-	}
+	return connections
+}
 
-	for i := 0; i < configData.Acceptors.Count; i++ {
-		ip := strconv.Itoa(ipMin + ipMax)
-		port := strconv.Itoa(portMin + portMax)
-		CONNECTIONS = append(CONNECTIONS, ip+":"+port)
-	}
-
-	return CONNECTIONS
+//nextIP returns the adjacent ipv4 address
+func nextIP(ip net.IP, inc uint) net.IP {
+	i := ip.To4()
+	v := uint(i[0])<<24 + uint(i[1])<<16 + uint(i[2])<<8 + uint(i[3])
+	v += inc
+	v3 := byte(v & 0xFF)
+	v2 := byte((v >> 8) & 0xFF)
+	v1 := byte((v >> 16) & 0xFF)
+	v0 := byte((v >> 24) & 0xFF)
+	return net.IPv4(v0, v1, v2, v3)
 }
