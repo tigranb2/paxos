3e48b4661c0c022f2422ab6a7b09e8599adf582f
Tigran Bantikyan <tigranbantikyan@gmail.com>
2021-08-18 15:07:59 -0400

    added MsgProposer rpc

--- network/server.go
+++ network/server.go
@@ -11,21 +11,54 @@ import (
 
 type server struct {
 	msg.UnimplementedMessengerServer
-	acceptor *Acceptor
+	node interface{}
 }
 
-func (s *server) SendMsg(ctx context.Context, msg *msg.MsgSlice) (*msg.MsgSlice, error) {
-	fmt.Printf("Received message %+v\n", msg)
-	return s.acceptor.acceptMsg(msg), nil
-}
-
-func serverInit(a *Acceptor) {
-	ln, err := net.Listen("tcp", a.Ip)
-	if err != nil {
-		log.Fatalf("failed to start server: %v", err)
+func (s *server) MsgAcceptor(ctx context.Context, msg *msg.Msg) (*msg.Msg, error) {
+	acceptor, ok := s.node.(*Acceptor)
+	if !ok {
+		log.Fatalf("destination server must be an acceptor")
 	}
 
-	s := server{acceptor: a}
+	fmt.Printf("Received message %+v\n", msg)
+	return acceptor.acceptMsg(msg)
+}
+
+func (s *server) MsgProposer(ctx context.Context, request *msg.QueueRequest) (*msg.RequestResponse, error) {
+	proposer, ok := s.node.(*Proposer)
+	if !ok {
+		log.Fatalf("destination server must be a proposer")
+	}
+
+	proposer.clientRequest <- request
+	select {
+	case <-proposer.clientRequest:
+		return &msg.RequestResponse{RequestReceived: true}, nil
+	case <-ctx.Done():
+		return nil, ctx.Err()
+	}
+}
+
+func serverInit(node interface{}) {
+	var ln net.Listener
+	var err error
+	var s server
+
+	switch t := node.(type) {
+	case *Acceptor:
+		s = server{node: t}
+		ln, err = net.Listen("tcp", t.ip)
+		if err != nil {
+			log.Fatalf("failed to start server: %v", err)
+		}
+	case *Proposer:
+		s = server{node: t}
+		ln, err = net.Listen("tcp", t.ip)
+		if err != nil {
+			log.Fatalf("failed to start server: %v", err)
+		}
+	}
+
 	grpcServer := grpc.NewServer()
 	msg.RegisterMessengerServer(grpcServer, &s)
 	if err := grpcServer.Serve(ln); err != nil {
