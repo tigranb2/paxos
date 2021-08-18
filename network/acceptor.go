0e9c5b7150f9c8bc9d986731013a131e71d50063
Tigran Bantikyan <tigranbantikyan@gmail.com>
2021-08-18 15:10:30 -0400

    handle msgs individually

--- network/acceptor.go
+++ network/acceptor.go
@@ -1,12 +1,13 @@
 package network
 
 import (
+	"errors"
 	"paxos/msg"
 )
 
 type Acceptor struct {
-	Ip           string
 	acceptorData map[int32]*consensusData
+	ip           string //socket for Acceptor server
 }
 
 type consensusData struct {
@@ -14,47 +15,42 @@ type consensusData struct {
 	accepted string
 }
 
-func InitAcceptor(ip string, instances int) Acceptor {
-	aData := make(map[int32]*consensusData)
-	for i := 1; i <= instances; i++ {
-		aData[int32(i)] = &consensusData{}
-	}
-	return Acceptor{Ip: ip, acceptorData: aData}
+func InitAcceptor(ip string) Acceptor {
+	return Acceptor{ip: ip, acceptorData: make(map[int32]*consensusData)}
 }
 
 func (a *Acceptor) Run() {
 	serverInit(a)
 }
 
-func (a *Acceptor) acceptMsg(rec *msg.MsgSlice) *msg.MsgSlice {
-	resp := msg.MsgSlice{Msgs: make(map[int32]*msg.Msg)}
-	for instance, recMsg := range rec.Msgs {
-		aData := a.acceptorData[instance]
-		var r msg.Msg
-		switch recMsg.GetType() {
-		case msg.Type_Prepare:
-			//promise to ignore ids lower than received id
-			if recMsg.GetId() > aData.promised {
-				if aData.accepted != "" { //case where node has accepted prior value
-					aData.promised = recMsg.GetId()
-					r = msg.Msg{Type: msg.Type_Promise, Id: recMsg.GetId(), Value: aData.accepted, PreviousId: aData.promised}
-				} else {
-					aData.promised = recMsg.GetId()
-					r = msg.Msg{Type: msg.Type_Promise, Id: recMsg.GetId()}
-				}
-			} else {
-				r = msg.Msg{Type: msg.Type_Promise, Id: 0}
-			}
-		case msg.Type_Propose:
-			//accept proposed value
-			if aData.promised == recMsg.GetId() {
-				aData.accepted = recMsg.GetValue()
-				r = msg.Msg{Type: msg.Type_Accept, Id: recMsg.GetId(), Value: recMsg.GetValue()}
-			} else {
-				r = msg.Msg{Type: msg.Type_Promise, Id: 0}
-			}
-		}
-		resp.Msgs[instance] = &r
+func (a *Acceptor) acceptMsg(rec *msg.Msg) (r *msg.Msg, err error) {
+	if _, ok := a.acceptorData[rec.GetSlotIndex()]; !ok {
+		a.acceptorData[rec.GetSlotIndex()] = &consensusData{}
 	}
-	return &resp
+
+	acceptorSlotData := a.acceptorData[rec.GetSlotIndex()]
+	switch rec.GetType() {
+	case msg.Type_Prepare:
+		//promise to ignore ids lower than received id
+		if rec.GetId() > acceptorSlotData.promised {
+			if acceptorSlotData.accepted != "" { //case where node has accepted prior value
+				acceptorSlotData.promised = rec.GetId()
+				r = &msg.Msg{Type: msg.Type_Promise, SlotIndex: rec.GetSlotIndex(), Id: rec.GetId(), Value: acceptorSlotData.accepted, PreviousId: acceptorSlotData.promised}
+			} else {
+				acceptorSlotData.promised = rec.GetId()
+				r = &msg.Msg{Type: msg.Type_Promise, SlotIndex: rec.GetSlotIndex(), Id: rec.GetId()}
+			}
+		} else {
+			err = errors.New("promised higher id")
+		}
+	case msg.Type_Propose:
+		//accept proposed value
+		if acceptorSlotData.promised == rec.GetId() {
+			acceptorSlotData.accepted = rec.GetValue()
+			r = &msg.Msg{Type: msg.Type_Accept, SlotIndex: rec.GetSlotIndex(), Id: rec.GetId(), Value: rec.GetValue()}
+		} else {
+			err = errors.New("promised different id")
+		}
+	}
+	return r, err
 }
